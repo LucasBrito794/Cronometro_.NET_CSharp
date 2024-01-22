1708369b5e2298da0a7c51095cf0dd7b108e2560
Lucas Martins <lucasmartinsemp794@gmail.com>
2024-01-22 10:35:07 -0300

    Cronômetro em .NET C#

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Stopwatch {
+    class Program {
+        static void Main(string [] args) {
+            Menu();
+        }
+    
+        static void Menu() {
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minutos => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.Write("Quanto tempo deseja contar? ");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(type == 'm') 
+                multiplier = 60;
+            
+            if(time == 0) 
+                Sair(time);
+
+            PreStart(time * multiplier);
+
+        }    
+        static void PreStart(int time) {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(1000);
+
+            Start(time);
+        }
+        static void Start(int time) {
+            int currentTime = 0;
+
+            while(currentTime != time) {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2000);
+            Menu();
+        }
+    
+        static void Sair(int time) {
+            if(time == 0) {
+                Console.Clear();
+                Console.WriteLine("Stopwatch finalizado!");
+                Thread.Sleep(1000);
+                System.Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+<div align = "center"> 
+    <h1>Cronômetro em .NET C#</h1>
+</div>
+
+### 🖥 Sobre o Projeto
+- Aplicação desenvolvida para praticar a sintaxe do .NET C#;
+- Desenvolvi um cronômetro que realiza sua contagem em segundos ou minutos, deleguei as funcionalidades em Funções com Menu, PreStart, Start e Saída do programa.
+- Aplicação para console.
+
+### 🌟 Tecnologias utilizadas
+- [.NET C#](https://learn.microsoft.com/pt-br/dotnet/csharp/)
+
+### ⚙️ Execução da aplicação
+- [Vídeo com execução da aplicação](https://github.com/LucasBrito794/Calculadora_.NET_CSharp/assets/78000311/d88a40fa-66fb-4900-862c-11703f1d0980
+)
+
+### 🎯 Imagens do projeto
+![image](https://github.com/LucasBrito794/Calculadora_.NET_CSharp/assets/78000311/d955929b-5acf-437f-a636-ed9138543048)
+
+##
+
+![image](https://github.com/LucasBrito794/Calculadora_.NET_CSharp/assets/78000311/fca08bde-18a0-4a1b-add8-88c9e8c82613)
+
